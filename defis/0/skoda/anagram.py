8318574fbd626362a7018d64c782e33c362f35fd
jskd <git@jeromeskoda.fr>
2018-01-08 12:36:18 +0100

    Fix anagram

--- defis/0/skoda/anagram.py
+++ defis/0/skoda/anagram.py
@@ -2,9 +2,9 @@ import sys
 
 # Ché pas se ke je kode!
 
-def isAnagam(str1, str2):
-    for s in list(str1):
-        if s not in list(str2):
+def isAnagram(str1, str2):
+    for s in list(str1.lower()):
+        if s not in list(str2.lower()):
             return False
     return True
 
@@ -15,5 +15,5 @@ if(len(sys.argv) < 2):
 with open(sys.argv[1]) as f:
     for word in f.readlines():
         for anagram in sys.argv[2::]:
-            if isAnagam(word, anagram):
+            if isAnagram(word, anagram):
                 print(word)
