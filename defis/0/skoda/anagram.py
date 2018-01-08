7d2663357bd07c24339df5ecb4307de499b31575
jskd <git@jeromeskoda.fr>
2018-01-08 12:23:43 +0100

    anagram v1

---
+++ defis/0/skoda/anagram.py
@@ -0,0 +1,17 @@
+import sys
+
+def isAnagam(str1, str2):
+    for s in list(str1):
+        if s not in list(str2):
+            return False
+    return True
+
+if(sys.argv < 2):
+    print("Wesh, usage: anagram mydict foo bar")
+    sys.exit()
+
+with open(sys.argv[1]) as f:
+    for word in f.readlines():
+        for anagram in sys.argv[2::]
+            if isAnagram(word, anagram):
+                print(word)
