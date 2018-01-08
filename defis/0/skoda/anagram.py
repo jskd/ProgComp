15f3d0fc827981ab72b42e18f402770e60f50838
jskd <git@jeromeskoda.fr>
2018-01-08 12:30:59 +0100

    Fix unorderable types: list() < int()

--- defis/0/skoda/anagram.py
+++ defis/0/skoda/anagram.py
@@ -1,17 +1,19 @@
 import sys
 
+# Ché pas se ke je kode!
+
 def isAnagam(str1, str2):
     for s in list(str1):
         if s not in list(str2):
             return False
     return True
 
-if(sys.argv < 2):
+if(len(sys.argv) < 2):
     print("Wesh, usage: anagram mydict foo bar")
     sys.exit()
 
 with open(sys.argv[1]) as f:
     for word in f.readlines():
-        for anagram in sys.argv[2::]
-            if isAnagram(word, anagram):
+        for anagram in sys.argv[2::]:
+            if isAnagam(word, anagram):
                 print(word)
