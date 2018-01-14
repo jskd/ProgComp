92d26e19bb513c966a95de8b33a3f89d0e2c9567
Thomas HAUTIER <hautier@informatique.univ-paris-diderot.fr>
2018-01-14 18:29:32 +0100

    Version 2

--- defis/0/hautier/anagram.py
+++ defis/0/hautier/anagram.py
@@ -7,30 +7,14 @@ def read_file(filename):
             res.append(line.strip())
     return res
 
-def permut(word,i, j):
-    liststring = []
-    for a in word:
-        liststring+=a
-    tmp = liststring[j]
-    liststring[j] = liststring[i]
-    liststring[i] = tmp
-
-    return ''.join(liststring)
-
-def genanagram(w):
-    anagramlist = set() 
-    for a in range(len(w)):
-        for b in range(len(w)):
-            anagramlist.add(permut(w, a, b))
-    return anagramlist
-
 words = []
 dic = read_file(sys.argv[1])
 
 for a in range(2, len(sys.argv)):
-    words.append(list(genanagram(sys.argv[a])))
+    words.append(sys.argv[a])
+
 
 for k in dic:
     for g in words:
-        if k in g:
+        if ''.join(sorted(k)) == ''.join(sorted(g)):
             print(k)
