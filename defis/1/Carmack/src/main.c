1db8be830d958a44c4f186b3126526499cc344f4
fedor <railachos@gmail.com>
2018-01-17 20:39:25 +0100

    Add our glorious structure.

--- defis/1/Carmack/src/main.c
+++ defis/1/Carmack/src/main.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 #include <errno.h>
 
+typedef enum { FORMULA, VALUE } data_ty;
+
+typedef struct {
+    int r1, r2, c1, c2, val;
+} formula;
+
+typedef struct {
+    data_ty ty;
+    union {
+        int value;
+        formula formula;
+    };
+} data;
+
 int main(int argc, const char* argv[]) {
 	if (argc != 5) {
 		errno = EINVAL;
