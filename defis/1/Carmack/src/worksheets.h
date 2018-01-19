86a699c927c7007633b820a192503c033923f173
Yves Viaud <mcnight@mcnight.fr>
2018-01-19 19:14:08 +0100

    User parsing

--- defis/1/Carmack/src/worksheets.h
+++ defis/1/Carmack/src/worksheets.h
@@ -3,6 +3,12 @@
 
 // Data types
 
+// Some data types may be unnecessary for the moment
+// but keep in mind that it is a naive implementation.
+// Refactor if you want to but please do not loose semantic value !
+// Besides, if we implement a big data worksheet engine in the future,
+// we may need them for storing multiple metadata.
+
 typedef enum { INVALID, FORMULA, VALUE } data_ty;
 
 typedef struct {
@@ -27,15 +33,35 @@ typedef struct {
 	line* lines;
 } worksheet;
 
+typedef struct {
+	int r, c;
+	data value;
+} user_component;
+
+typedef struct {
+	unsigned changes_count;
+	user_component* content;
+} user;
+
 // Functions
 
+/// Parsing
 int parse_data(const char* path, worksheet* ws);
 int parse_formula(char* token, data* formula);
+int parse_user(const char* path, user* user_mods);
 
+/// Worksheet/User management
+void evaluate_worksheet(worksheet* ws); // Maybe add others parameters ?
+void apply_user(worksheet* ws);
 void release_worksheet(worksheet* ws);
+void release_user(user* user_mods);
 
+/// Print things
 void print_worksheet(worksheet* ws);
+void print_user(user* user_mods);
+void print_data(data* value, const char* separator);
 
+/// Utilities
 data_ty token_type(const char* token);
 
 #endif
