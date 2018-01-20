7a16a1c87665eb577016ac613be4ba2d3913be8a
McNight <mcnight@mcnight.fr>
2018-01-20 14:49:54 +0100

    Error handling

--- defis/1/Carmack/src/main.c
+++ defis/1/Carmack/src/main.c
@@ -15,8 +15,16 @@ int main(int argc, const char* argv[]) {
 	struct worksheet current_worksheet;
 	struct user_data current_user;
 
-	parse_data(argv[1], &current_worksheet);
-	parse_user(argv[2], &current_user);
+	if (parse_data(argv[1], &current_worksheet) != 0) {
+		fprintf(stderr, "Error while parsing `data.csv` file");
+		return EXIT_FAILURE;
+	}
+
+	if (parse_user(argv[2], &current_user) != 0) {
+		fprintf(stderr, "Error while parsing `user.txt` file");
+		return EXIT_FAILURE;
+	}
+
 	print_worksheet(&current_worksheet);
 	print_user(&current_user);
 	release_worksheet(&current_worksheet);
