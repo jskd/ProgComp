087288742296df58311c620534998c4527945e30
Hocine <oulebsir@informatique.univ-paris-diderot.fr>
2018-01-20 18:13:39 +0100

    [Scableur] Récuperation section tableur

--- defis/1/Scableur/src/main/scala/prog/comp2018/scableur/data/ViewBuilder.scala
+++ defis/1/Scableur/src/main/scala/prog/comp2018/scableur/data/ViewBuilder.scala
@@ -1,24 +1,33 @@
 package prog.comp2018.scableur.data
 
+import java.util
 import java.util.ArrayList
 
-import prog.comp2018.scableur.file.FileReader
-import prog.comp2018.scableur.utils.Conf
+
+import prog.comp2018.scableur.utils.{Conf, FileReader}
 
 object ViewBuilder {
   // Build a Partial View from data
-  def build(i1: Int, j1: Int, i2: Int, j2 : Int ) : ArrayList[ArrayList[Value]] = {
-    //TODO load partie partiel de data.csv
-    val f : FileReader = new FileReader(Conf.InputData.dataFile)
-    var s : String  = f.line
 
-    var cptline : Int =0
+  def getsection(str: String, i: Int, j:Int):ArrayList[String] ={
+    return new util.ArrayList[String]()//////TODO
+  }
+
+  def build(i1: Int, j1: Int, i2: Int, j2 : Int ) : ArrayList[ArrayList[String]] = {
+    //TODO load partie partiel de data.csv
+    val matrix : ArrayList[ArrayList[String]] = new ArrayList[ArrayList[String]]()
+
+    var s : String =""
+    
+    for(i <- i1  to i2+1 ){
+      s=FileReader.getNthLine(Conf.InputData.dataFile,i)
+      matrix.add(getsection(s, j1,j2))
+
+    }
+
+
     
 
-    while (s != null){
-      println(s)
-      s = f.line
-    }
-    null
+    matrix
   }
 }
