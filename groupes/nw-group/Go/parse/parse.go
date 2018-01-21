f02981faa3ff9861085435f50fc311ba4aaf588e
krimo <hamadikrimo@hotmail.com>
2018-01-21 11:06:15 +0100

    file parsing V2 and write view0.csv : misses evaluation

--- groupes/nw-group/Go/parse/parse.go
+++ groupes/nw-group/Go/parse/parse.go
@@ -2,17 +2,47 @@ package main
 
 import ("fmt"
 		 "io/ioutil"
+		 "os"
 		 "strings")
 
-func main() {
-	file, err := ioutil.ReadFile("data.csv")
+func checkError(err error){
 	if err != nil {
 		 panic(err)
 	}
+}
+
+/*read File and returns there content in a 2D slice of strings*/
+func readFile() [][]string{
+	file, err := ioutil.ReadFile("data.csv")
+	checkError(err)
    lines := strings.Split(string(file), "\n")
-	fmt.Printf("%q\n", lines)
+	car:=make([][]string, 0, 0)
 	for i:=0;i<len(lines);i++ {
-		fmt.Printf("%q\n",strings.Split(lines[i], ","))
-		//car:=strings.Split(lines[i], ",")
+		car=append(car,strings.Split(lines[i], ","))
 	}
+	return car
+}
+/*write in File the 2D slice of strings*/
+func writeFile(){
+	taille := len(readFile())
+	s:=make([][]string, taille,cap(readFile()))
+	s=readFile()
+	fmt.Printf("%q\n",s)
+	f, err := os.OpenFile("view0.csv", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		panic(err)
+	}
+	sep := "\n"
+	for i:=0;i<len(s);i++ {
+		for j:=0;j<cap(s);j++ {
+			_, err = f.WriteString(s[i][j])
+			checkError(err)
+		}
+		_, err = f.WriteString(sep)
+		checkError(err)
+	}
+}
+
+func main() {
+	writeFile()
 }
