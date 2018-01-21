1ca7bf717b8982a743ef147fcd83a7da90315e4f
CactusTribe <lefrancjoaquim@gmail.com>
2018-01-21 14:17:39 +0100

    Refactoring et structuration des tests

--- defis/1/la_prosperite_des_charlatans/proto.py
+++ defis/1/la_prosperite_des_charlatans/proto.py
@@ -1,11 +1,41 @@
+#!/usr/bin/env python3
+
 import json
-import os
+import os,sys
+from subprocess import *
 
-json_data=open("defis/1/la_prosperite_des_charlatans/target.json")
-data = json.load(json_data)
-json_data.close()
+if __name__ == "__main__":
 
-for target in data["target"]:
-  print (target["name"])
-  print ("TEST SUCESS -> NOTE: 20/20 avec félicitations du jury")
-  print ()
+    with open("target.json") as data:
+        data_targets = json.load(data)
+
+    with open("tests.json") as data:
+        data_tests = json.load(data)
+
+
+    for target in data_targets["target"]:
+        print(" # Current project :", target["name"])
+
+        for i, test in enumerate(data_tests["tests"]):
+
+            cmd = test["exec"]
+
+            if cmd == "" :
+                # Here, tests without exec files
+                # out = anotherFunction()
+                out = ""
+            else:
+                out = Popen(cmd, stdout=PIPE).communicate()[0].decode("utf-8")
+
+            if test["expected"] != "":
+                with open(test["expected"], 'r') as f:
+                    result = "PASS" if f.read() == out else "FAIL"
+            else:
+                # Here, tests without expected files
+                # result = anotherExpectedThings
+                result = "FAIL"
+
+            print(" [{}] -> [{}] {} {}".format(i, result, test["name"], test["expected"]))
+
+    print ("\n >> RESULT : [{}/{}] -> NOTE: 20/20 avec félicitations du jury".format(0, len(data_tests["tests"])))
+    print ()
