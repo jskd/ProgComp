7bc2c18583fbfaf67fbb0a2d96640d1006dbc236
Xiang LI <xiangfr007@gmail.com>
2018-01-22 11:45:58 +0100

    test marche pas

--- defis/1/la_prosperite_des_charlatans/proto2.py
+++ defis/1/la_prosperite_des_charlatans/proto2.py
@@ -18,20 +18,20 @@ if __name__ == "__main__":
         print("-"*32)
 
         nb_passed = 0
-
+        Popen(["ws", "tests/data.csv", "tests/user.txt",target["path"]+  "view0.csv", target["path"]+ "changes.txt"], stdout=PIPE).communicate()[0].decode("utf-8")
 
         for i, test in enumerate(data_tests["tests"]):
             test_file = target["path"] +"/"+test["expected"]
-            cmd = test["exec"]
-            cmd.append(test_file)
+            cmd = ["diff",test_file ,test["file"] ]
             print(cmd)
+            out = Popen(cmd, stdout=PIPE).communicate()[0].decode("utf-8")
             """
             if cmd == "" :
                 # Here, tests without exec files
                 # out = anotherFunction()
                 out = "0 0 5\n1 0 10\n1 1 =#(0, 0, 1, 0, 5)"
             else:
-                out = Popen(cmd, stdout=PIPE).communicate()[0].decode("utf-8")
+                
 
             if test["expected"] != "":
                 with open(test["expected"], 'r') as f:
