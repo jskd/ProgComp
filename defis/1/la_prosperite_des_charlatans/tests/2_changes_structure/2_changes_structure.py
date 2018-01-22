8179f754bcfc37d03657925ed71605adc0f1be13
CactusTribe <lefrancjoaquim@gmail.com>
2018-01-22 18:17:55 +0100

    Ajout de tout les tests de structure des fichiers input

---
+++ defis/1/la_prosperite_des_charlatans/tests/2_changes_structure/2_changes_structure.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import json
+import os,sys
+from subprocess import *
+from os import listdir
+import os.path
+from os.path import isfile, join
+import re
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 1:
+        path = os.path.dirname(sys.argv[0]) + "/"
+        tested_path = sys.argv[1]
+
+        # INIT DATA
+        with open(path  + "infos.json", 'r') as data:
+            test_data = json.load(data)
+            test_info = test_data["infos"][0]
+
+        test_result = True
+
+        try:
+            with open(tested_path + "/" + "changes.txt", "r") as user_file:
+
+                for line in user_file.readlines():
+                    line_splited = line.strip("\n").split(" ")
+
+                    matchObj = re.match(r"(\d.*) (\d.*) (\d.*|=#\(\d.*,\d.*,\d.*,\d.*,\d.*\)|P)(:?)", line)
+
+                    if not matchObj:
+                        test_result = False
+                        break
+
+
+        except: test_result = False
+        # BEGIN OF SCRIPT
+        print(test_result) # True for PASS, False for FAIL
+        # END OF SCRIPT
+
+    else:
+        print("Usage: files_presence.py <expected_file>")
