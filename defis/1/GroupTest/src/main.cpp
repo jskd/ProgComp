e87ba72d28558e66060d90a327325ed1c066486a
CactusTribe <lefrancjoaquim@gmail.com>
2018-01-25 01:05:53 +0100

    Refactoring et ajout du test d'execution de ws

--- defis/1/GroupTest/src/main.cpp
+++ defis/1/GroupTest/src/main.cpp
@@ -3,8 +3,16 @@
 using namespace std;
 
 int main(int argc, char** argv){
+  if(argc >= 4){
+    char* data_path = argv[0];
+    char* user_path = argv[1];
+    char* view0_path = argv[2];
+    char* changes_path = argv[3];
 
-  cout << "Hello world !" << endl;
-
+    cout << "Compute many things..." << endl;
+  }
+  else{
+    cout << "Usage: ws <data> <user> <view0> <changes>" << endl;
+  }
   exit(0);
 }
