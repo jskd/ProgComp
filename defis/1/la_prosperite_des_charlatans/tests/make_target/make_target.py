4aaf11bc0f7482b847ac6e3276e756e5666e849a
CactusTribe <lefrancjoaquim@gmail.com>
2018-01-27 13:23:40 +0100

    Scableur Compilation Test ok

--- defis/1/la_prosperite_des_charlatans/tests/make_target/make_target.py
+++ defis/1/la_prosperite_des_charlatans/tests/make_target/make_target.py
@@ -26,13 +26,13 @@ if __name__ == "__main__":
         # BEGIN OF SCRIPT
         test_result = True
         try:
-            if not os.path.isdir(group_path + BIN_PATH):
-                raise Exception("Error: bin/ does not exist.")
+            #if not os.path.isdir(group_path + BIN_PATH):
+            #    raise Exception("Error: bin/ does not exist.")
 
             with open(group_path + MAKEFILE_PATH, "r"): pass
 
             cmd = ["make", "--directory=" + group_path]
-            out = check_output(cmd, stderr=STDOUT, timeout=30).decode("utf-8")
+            out = check_output(cmd, stderr=STDOUT, timeout=60).decode("utf-8")
 
             matchObj = re.match(r"make\[1\]*", out)
             if matchObj:
