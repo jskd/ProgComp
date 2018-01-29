d01e247747bdce8ac780bfb60ef35696f1c930fd
Hocine <oulebsir@informatique.univ-paris-diderot.fr>
2018-01-29 11:17:09 +0100

    [Scableur] Matrix

--- defis/1/Scableur/src/main/scala/prog/comp2018/scableur/data/Matrix.scala
+++ defis/1/Scableur/src/main/scala/prog/comp2018/scableur/data/Matrix.scala
@@ -1,8 +1,9 @@
 package prog.comp2018.scableur.data
 
 import com.github.tototoshi.csv.CSVReader
+import prog.comp2018.scableur.data.functions.NbrIteration
 
-
+import scala.util.matching.Regex
 import scala.collection.mutable.ArrayBuffer
 
 class Matrix(private var filename: String) {
@@ -33,6 +34,35 @@ class Matrix(private var filename: String) {
 
   def StringToValue(lls : List[List[String]]) : ArrayBuffer[ArrayBuffer[Value]] = {
 
+    val pattern = """[#=(](\d+),(\d+),(\d+),(\d+),(\d+)[)]""".r
+
+    var str= ""
+
+    for(i<-0 to (lls.length)){
+      for(j<-0 to (lls(0).length)){
+
+        if(lls(i)(j)(0)=='#'){
+          str=lls(i)(j)
+          val parts=pattern.findAllIn(str)
+          val i1=parts.group(1)
+          val j1=parts.group(2)
+          val i2=parts.group(3)
+          val j2=parts.group(4)
+          val value=parts.group(5)
+
+          matrix(i)(j)=new NbrIteration(Array(i1.toInt,j1.toInt,i2.toInt,j2.toInt),new ConstantType(Some(value.toInt)))
+
+        }
+
+        else{
+          matrix(i)(j)=new ConstantType(Some(lls(i)(j).toInt))
+
+        }
+
+      }
+
+
+    }
 
     return matrix
 
