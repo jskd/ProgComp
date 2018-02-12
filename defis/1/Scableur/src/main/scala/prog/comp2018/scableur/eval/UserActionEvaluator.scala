ae463166d23e7105493be6e69fd5c5d23008c7d7
Nicolas Hamilakis @PBW <nick.hamilakis562@gmail.com>
2018-02-12 10:45:50 +0100

    [Scableur] Changes printer

--- defis/1/Scableur/src/main/scala/prog/comp2018/scableur/eval/UserActionEvaluator.scala
+++ defis/1/Scableur/src/main/scala/prog/comp2018/scableur/eval/UserActionEvaluator.scala
@@ -1,27 +1,37 @@
 package prog.comp2018.scableur.eval
 
 import prog.comp2018.scableur.data._
+import prog.comp2018.scableur.data.functions.NbrIteration
 
 import scala.collection.mutable.ListBuffer
 
 class UserActionEvaluator(val matrix: Matrix, val source : UserData) extends Evaluator[ChangesData] {
   private val changesData : ChangesData = new ChangesData()
   var functionStack : ListBuffer[FunctionType] = ListBuffer[FunctionType]()
+  var lstfun : List[Int] = List[Int]()
 
   def isInRange(pos : (Int,Int), x1 : (Int,Int), x2: (Int, Int)) : Boolean = {
     pos._1 >= x1._1 && pos._1 <= x2._1 && pos._2 >= x1._2 && pos._2 <= x2._2
   }
 
   def testFunctionChanges(coord: (Int,Int)) : Unit = {
-    for(fun <- matrix.functionStack if isInRange(coord, fun.from,fun.to))
-      functionStack.+=:(fun) //FIXME: no duplicates
-
+    for(i <- Range(0,matrix.functionStack.length,1)) {
+      val fun = matrix.functionStack(i)
+      if(isInRange(coord, fun.from, fun.to) && !lstfun.contains(i) ){
+        functionStack.+=:(fun)
+        lstfun = i :: lstfun
+      }
+    }
   }
 
   def constantEval(v: ConstantType) : Option[Int] = v.value
 
   def evalFunctionStack() : Unit = {
-    //TODO same method as MatrixEvaluator
+    for(f <- functionStack){
+      f match  {
+        case f1 : NbrIteration => changesData.modificationList.+=:(f1.coordinates, NbrIterationEval.eval(f1,matrix))
+      }
+    }
   }
 
   override def eval: ChangesData = {
