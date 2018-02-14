16a9f407a376b9f4fa66eef2b2fee300904f36a6
CactusTribe <lefrancjoaquim@gmail.com>
2018-02-14 13:17:51 +0100

    Premiere connexion a la BD sur Index.html

--- defis/1/la_prosperite_des_charlatans/web_app/Controllers/Home.py
+++ defis/1/la_prosperite_des_charlatans/web_app/Controllers/Home.py
@@ -1,9 +1,33 @@
-import cherrypy
+import cherrypy, sqlite3
 from Controllers.Base import BaseController
 import os
 
+DB_PATH = "data/database.db"
 
 class HomeController(BaseController):
     @cherrypy.expose
     def index(self):
-        return self.render_template({"pwd":os.getcwd()})
+        lasts_passed = self.get_lasts_passed()
+        return self.render_template({"last_group_test":lasts_passed})
+
+    def get_lasts_passed(self):
+        try:
+            conn = sqlite3.connect(DB_PATH)
+            cursor = conn.cursor()
+
+            #qry = "SELECT sha, date_test, name_group, result, COUNT() FROM logs GROUP BY sha, name_group \
+            #    ORDER BY date_test DESC LIMIT 10;".format()
+
+            qry = "SELECT substr(sha, -8) as commit_sha, date_test, name_group, result, sum(case when result='PASS' then 1 else 0 end), COUNT(*) FROM logs\
+                GROUP BY name_group, commit_sha ORDER BY date_test DESC;".format()
+
+            cursor.execute(qry)
+            result_qry = cursor.fetchall()
+
+        except Exception as e:
+            print(str(e))
+            conn.rollback()
+        finally:
+            cursor.close()
+            conn.close()
+            return result_qry
