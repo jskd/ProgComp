2e29168291ba70de07d453589fea18b783ff5f87
FLECHEUX joan <flecheux@informatique.univ-paris-diderot.fr>
2018-02-14 10:35:05 +0100

    *Harmonisation de parser.rs avec le code deja nettoye. *MaJ de TODO en consequence.

--- defis/1/babar/src/parser.rs
+++ defis/1/babar/src/parser.rs
@@ -3,8 +3,8 @@ use std::fs::File;
 
 use cell;
 
-
-pub fn read_file(f :&str) -> String
+///Opens the file f and returns its content as a String.
+pub fn read_file(f: &str) -> String
 {
     let mut file = File::open(f).expect("Error Opening data.csv");
     let mut data = String::new();
@@ -12,33 +12,37 @@ pub fn read_file(f :&str) -> String
     return data.trim().to_string();
 }
 
+///Creates a new cell containing the formula given in parameter.
 pub fn init_formula(form_dec_vec: Vec<&str>) -> Box<cell::Cell>
 {
     let cell = cell::Formula{
-            num:0,
-            r1:form_dec_vec[0].trim().parse()
+            num: 0,
+            r1: form_dec_vec[0].trim().parse()
             .expect("Erreur format"),
-            c1:form_dec_vec[1].trim().parse()
+
+            c1: form_dec_vec[1].trim().parse()
             .expect("Erreur format"),
-            r2:form_dec_vec[2].trim().parse()
+
+            r2: form_dec_vec[2].trim().parse()
             .expect("Erreur format"),
-            c2:form_dec_vec[3].trim().parse()
+
+            c2: form_dec_vec[3].trim().parse()
             .expect("Erreur format"),
-            val:form_dec_vec[4].trim().parse()
+
+            val: form_dec_vec[4].trim().parse()
             .expect("Erreur format"),
     };
     Box::new(cell)
-         
 }
 
+//TODO
 pub fn create_cell(str:String) -> Box<cell::Cell>
 {
     if Some('=') == str.chars().next() {
-        let form : String = str.trim_matches(|c| c == '(' || c == ')' 
-         || c == '=' || c == '#' ).to_string();
-         let form_decompose = form.split(",");
-         let form_dec_vec: Vec<&str> = form_decompose.collect();
-         if form_dec_vec.len() < 5 {
+        let form : String = str.trim_matches(|c| c == '(' || c == ')' || c == '=' || c == '#' ).to_string();
+        let form_decompose = form.split(",");
+        let form_dec_vec: Vec<&str> = form_decompose.collect();
+        if form_dec_vec.len() < 5 {
             panic!("Erreur format");
          }
          
@@ -53,35 +57,37 @@ pub fn create_cell(str:String) -> Box<cell::Cell>
     }
     
 }
+
+///Generates a cell matrix from the data contained in the string passed in parameter.
 pub fn gen_table(data: String) -> Vec<Vec<Box<cell::Cell>>>
 {
-    let mut t = Vec::new();
-    let a =  data.split("\n");
-    let vec = a.collect::<Vec<&str>>();
-    for c in &vec{
-        let mut temp = c.split(";");
-        let  vec2 = temp.collect::<Vec<&str>>();
+    let mut table = Vec::new();
+    let data_str =  data.split("\n");
+    let data_collection = data_str.collect::<Vec<&str>>();
+    for row_str in &data_collection{
+        let mut temp = row_str.split(";");
+        let row_collection = temp.collect::<Vec<&str>>();
         let mut row = Vec::new();
-        for d in &vec2{
-            let cell = create_cell(d.to_string());
+        for elem in &row_collection{
+            let cell = create_cell(elem.to_string());
             row.push(cell);
-            
         }
-        t.push(row);
+        table.push(row);
     }
-    return t;
+    return table;
 }
-pub fn print_table(t:&Vec<Vec<Box<cell::Cell>>>)
+
+pub fn print_table(table: &Vec<Vec<Box<cell::Cell>>>)
 {
     let mut i:i32 = 0;
-    for k in t{
-        for b in k{
+    for row in table{
+        for elem in row{
             if i!=0
             {
                 print!(";");
             }
             i+=1;
-            print!("{}", b.get_value());
+            print!("{}", elem.get_value());
         }
         i=0;
         println!("");
