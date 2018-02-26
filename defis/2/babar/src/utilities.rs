40c0b1d99358f9a5e9a0f9c6188a300f8a9dd76b
DIABIRA binta <diabira@informatique.univ-paris-diderot.fr>
2018-02-26 12:57:45 +0100

    utiliteies

--- defis/2/babar/src/utilities.rs
+++ defis/2/babar/src/utilities.rs
@@ -1,32 +1,33 @@
-use std::io;
+use std;
+use std::io::{ BufRead};
 use std::fs::File;
-use std::io::BufReader;
 
+// mod parser;
 use cell;
+// mod treatment;
 
 
 pub const BUFF_SIZE: usize = 500;
 
 ///count bytes by lines, & get all formula in formula tab
-pub fn read_first_time(path: &str, bytes_by_lines: &Vec<usize>, formulas: &Vec<cell::Formula>)
+pub fn read_first_time(path: &str, bytes_by_lines: &mut Vec<usize>, formulas: &mut Vec<cell::Formula>)
 {
-	let file = File::open("foo.txt").expect("fail to open");
+	let file = File::open(path).expect("fail to open");
 	let mut buff = Vec::with_capacity(BUFF_SIZE);
-	let mut reader = BufReader::(file);
+	let mut reader = std::io::BufReader::new(file);
 	let mut num_bytes = reader.read_until(b'\n',&mut buff).expect("counting bytes in lines");
 	bytes_by_lines.push(num_bytes);
 	while num_bytes!=0 
 	{
-		has_formula(buff, formulas);                   //check if line contains formula, if so, add them to Formulas
-		reader.read_until(b'\n',&mut buff).expect("counting bytes in lines");
-		bytes_by_lines.push(bytes_by_lines.pop().unwrap() + num_bytes);
+		has_formula(&buff, &formulas);                   //check if line contains formula, if so, add them to Formulas
+		let br = reader.read_until(b'\n',&mut buff).expect("counting bytes in lines");
+//  		bytes_by_lines.push(bytes_by_lines.pop().unwrap() + num_bytes);
 
 	}
 
 
 
 }
-
 pub fn has_formula(line: Vec<u8>, formulas: &Vec<cell::Formula>)
 {
 	let lineproper = line.trim()
