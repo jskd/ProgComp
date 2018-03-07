4e1bb17c2d2d5e30d7314aabe07ef8062ed567b7
BONGRAND Guillaume <bongrand@informatique.univ-paris-diderot.fr>
2018-03-07 14:35:36 +0100

    first parsing done

--- defis/2/babar/src/utilities.rs
+++ defis/2/babar/src/utilities.rs
@@ -16,46 +16,31 @@ pub fn read_first_time(path: &str, formulas: &mut Vec<cell::Formula>)
 	let mut buff = Vec::with_capacity(BUFF_SIZE);
 	let mut reader = std::io::BufReader::new(file);
 	let mut num_bytes = reader.read_until(b'=',&mut buff).expect("read until formula");
+	buff.clear();
+	num_bytes=reader.read_until(b';',&mut buff).expect("read formula");
 	while num_bytes!=0 //Buffer not empty0
 	{
-
-		let mut index:usize = 0;
-		while has_formula(&buff){ //If buffer still got a formula
-			let mut formula: Vec<u8> = Vec::new();
-			formula.push(b'=');
-				for byte in &buff{
-					if byte == (&b';')
-					{
-						break;
-					}
-					else
-					{
-						formula.push(*byte);
-					}
+		let mut formula: Vec<u8> = Vec::new();
+		formula.push(b'=');
+			for byte in &buff{
+				if byte == (&b';')
+				{
+					break;
 				}
-			formulas.push(create_formula(String::from_utf8(formula).unwrap()));
-		}
-		num_bytes = reader.read_until(b'=',&mut buff).expect("read until formula or end file");
-	}
-}
-
-
-pub fn check_bounds(buff: &Vec<u8>, index: usize) -> bool
-{
-	match buff.get(index..){
-		Some(buff_ok) => {
-			match buff.iter().position(|&eq| eq == b'='){
-				Some(iter_ok) => true,
-				None => false,
+				else
+				{
+					formula.push(*byte);
+				}
 			}
-		},
-		None => false,
+		formulas.push(create_formula(String::from_utf8(formula).unwrap()));
+		num_bytes = reader.read_until(b'=',&mut buff).expect("read until formula or end file");
+		buff.clear();
+		num_bytes = reader.read_until(b';',&mut buff).expect("read file");
+		
+
 	}
 }
-pub fn has_formula(line: &Vec<u8>) -> bool
-{
- line.contains(&b'=')
-}
+
 
 pub fn create_formula(form_string: String) -> cell::Formula
 {
