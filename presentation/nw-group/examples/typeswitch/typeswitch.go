dc1e8a5ec3681b721c36649ac2b4d3515c88de42
Idir Lankri <lankri.idir@gmail.com>
2018-03-11 23:56:01 +0100

    nw-group/presentation: Add comment in code example

--- presentation/nw-group/examples/typeswitch/typeswitch.go
+++ presentation/nw-group/examples/typeswitch/typeswitch.go
@@ -1,3 +1,5 @@
+// Sum type and pattern matching simulation in Go using interfaces and
+// dynamic types.
 package main
 
 import "fmt"
@@ -25,12 +27,12 @@ func (e add) isExpr()     {}
 func (e mult) isExpr()    {}
 
 func toString(e expr) string {
-	switch e := e.(type) {
-	case literal:
+	switch e := e.(type) { // Analyze the type of e at runtime.
+	case literal: // Here e has type literal.
 		return fmt.Sprintf("%d", e.val)
-	case add:
+	case add: // Here e has type add.
 		return toString(e.left) + " + " + toString(e.right)
-	case mult:
+	case mult: // Here e has type mult.
 		return toString(e.left) + " * " + toString(e.right)
 	default:
 		panic("Unexpected expression.")
