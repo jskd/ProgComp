387ad538aff070376f401e92337cf1d2e5588a8a
FLECHEUX joan <flecheux@informatique.univ-paris-diderot.fr>
2018-03-25 15:22:07 +0200

    documention main

--- defis/2/babar/src/main.rs
+++ defis/2/babar/src/main.rs
@@ -1,3 +1,5 @@
+// For an explanation on how this project works, see pcomp/fonctionnement_projets/notes_rust.txt
+
 mod parser;
 mod processing;
 mod cell;
@@ -20,13 +22,10 @@ fn main() {
 
     /*
     let data = parser::read_file(&args[1]);
-
     let t = parser::gen_table(data); 
-
-    
     parser::print_table(&t);
     let (mut grid,mut dependences) = treatment::evaluate(&t);
     treatment::write_view0(&args[3],&grid);
     treatment::write_change(&args[2],&args[4],&mut grid,&mut dependences);
-*/
+    */
 }
